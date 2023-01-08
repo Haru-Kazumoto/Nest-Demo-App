eb4fff2a10b5dfa906cc04288962effc9738ae72
Ikhsan892 <mfatihul8902@gmail.com>
2023-01-08 13:49:32 +0700

    feat : module crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { UserSchema } from './model/user.models';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://nest-db:dINSWjcVzIOWQwqE@cluster0.y15gb9w.mongodb.net/?retryWrites=true&w=majority'
+      'mongodb+srv://nest-db:12345@cluster0.y15gb9w.mongodb.net/?retryWrites=true&w=majority'
     ),
     MongooseModule.forFeature([{name: 'user', schema:UserSchema}])
   ],
